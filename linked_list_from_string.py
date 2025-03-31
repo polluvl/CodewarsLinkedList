f5a239b8aa6ddded1b290c56e48be167902a9cf7
Polina Lukashenko <polinalukashenko12@gmail.com>
2025-03-31 11:11:22 +0300

    created a function, the output in codewars is almost the same, but has some minor problems

---
+++ linked_list_from_string.py
@@ -0,0 +1,31 @@
+'''linked_list_from_string'''
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def stringify(node):
+    outp = []
+    temp = node
+    while temp:
+        outp.append(str(temp.data))
+        temp = temp.next
+    if temp is None:
+        outp.append('None')
+    return ' -> '.join(outp)
+
+def linked_list_from_string(s):
+    spl = s.split(' -> ')
+    nodes = []
+    for i in spl:
+        nodes.append(i)
+    head = Node(nodes[0])
+    temp = head
+    for k in nodes[1:-1]:
+        temp.next = Node(k)
+        temp = temp.next
+    return head
+
+head = Node(0, Node(1, Node(4, Node(9, Node(16)))))
+print(stringify(head))
+print(linked_list_from_string('0 -> 1 -> 4 -> 9 -> 16 -> None'))
